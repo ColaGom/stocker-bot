7ded7c02e06a5fabac4746802afd39ecd37eab8a
ColaGom <bchoi000@gmail.com>
2021-01-17 03:46:47 +0900

    주식상장정보 API 구현

--- api.py
+++ api.py
@@ -11,17 +11,7 @@ KEY = os.environ.get("API_KEY")
 HOST = "http://api.seibro.or.kr/openapi/service/StockSvc/getShotnByMartN1"
 
 # 시장별 단축코드 전체 조회
-# ServiceKey
-# -
-# 공공데이터포털에서 받은 인증키
-# pageNo
-# 1
-# 페이지번호
-# numOfRows
-# 10
-# 한 페이지 결과 수
 # martTpcd
-# 11
 # 11.유가증권시장, 12.코스닥, 13.K-OTC, 14.코넥스, 50.기타시장
 
 
@@ -32,17 +22,46 @@ def stock_short(page_num, market):
     url = urljoin(HOST, "getShotnByMartN1")+'?'+query_string+'&serviceKey='+KEY
     response = requests.get(url)
     root = ET.fromstring(response.text)
+
     items = root.findall('.//items/item')
     print("Response : " + str(len(items)))
-    
+
     stocks = []
 
     for item in items:
-        id = item.find('shotnIsin').text
-        name = item.find('korSecnNm').text
-        stocks.append(ShortStockInfo(id, name))
+        stocks.append(Stock(item))
 
     for stock in stocks:
         print(stock)
 
     return stocks
+
+# 주식 기본 정보
+
+
+def dispatch_basic(stock):
+    queries = {"shortIsin": stock.short_id,
+               "numOfRows": 10, "pageNo": 1}
+    query_string = urlencode(queries)
+    url = urljoin(HOST, "getStkIsinByShortIsinN1") + \
+        '?'+query_string+'&serviceKey='+KEY
+    response = requests.get(url)
+    root = ET.fromstring(response.text)
+    element = root.find('.//items/item')
+    stock.append_basic(element)
+    return stock
+
+
+# 주식상장정보
+#
+def dispatch_listing_info(stock):
+    queries = {"isin": stock.id,
+               "numOfRows": 10, "pageNo": 1}
+    query_string = urlencode(queries)
+    url = urljoin(HOST, "getStkListInfoN1") + \
+        '?'+query_string+'&serviceKey='+KEY
+    response = requests.get(url)
+    root = ET.fromstring(response.text)
+    elements = root.findall('.//items/item')
+    stock.append_listing_info(elements)
+    return stock
--- entity.py
+++ entity.py
@@ -1,8 +1,28 @@
-class ShortStockInfo:
-    def __init__(self, id, name) -> None:
-        super().__init__()
-        self.id = id
-        self.name = name
+class Stock:
+    def __init__(self, element):
+        self.short_id = element.find('shotnIsin').text
+        self.name = element.find('korSecnNm').text
 
     def __str__(self) -> str:
-        return "["+self.id+"]" + self.name
+        return "["+self.short_id+"]" + self.name
+
+    def append_basic(self, element):
+        self.nameEng = element.find('engSecnNm').text
+        self.id = element.find('isin').text
+        self.issu_date = element.find('issuDt').text
+        self.issu_company_id = element.find('issucoCustno').text
+        self.stock_type = element.find('secnKacdNm').text
+
+    def append_listing_info(self, elements):
+        self.listing_info = []
+        for element in elements:
+            self.listing_info.append(ListingInfo(element))
+
+
+class ListingInfo:
+    def __init__(self, element):
+        self.listing_date = element.find('apliDt').text
+        self.closing_date = element.find('dlistDt').text
+        self.issu_company_id = element.find('issucoCustno').text
+        self.type = element.find('listTpcd').text
+        self.expired_at = element.find('xpitDt').text
